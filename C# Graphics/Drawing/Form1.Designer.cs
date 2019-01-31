19a1f091ce6f4a9ccc1a6c826b529b726e721163
qQuickz <46611059+qQuickz@users.noreply.github.com>
2019-01-31 17:02:44 +0100

    Add files via upload

---
+++ C# Graphics/Drawing/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ellipse = new System.Windows.Forms.Timer(this.components);
+            this.ReverseEllipse = new System.Windows.Forms.Timer(this.components);
+            this.VertikalLine = new System.Windows.Forms.Timer(this.components);
+            this.TotalGraph = new System.Windows.Forms.Timer(this.components);
+            this.HorizontalLine = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 590);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 570);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(12, 554);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // Ellipse
+            // 
+            this.Ellipse.Enabled = true;
+            this.Ellipse.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // ReverseEllipse
+            // 
+            this.ReverseEllipse.Enabled = true;
+            this.ReverseEllipse.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // VertikalLine
+            // 
+            this.VertikalLine.Enabled = true;
+            this.VertikalLine.Tick += new System.EventHandler(this.VertikalLine_Tick);
+            // 
+            // TotalGraph
+            // 
+            this.TotalGraph.Enabled = true;
+            this.TotalGraph.Interval = 1000;
+            this.TotalGraph.Tick += new System.EventHandler(this.TotalGraph_Tick);
+            // 
+            // HorizontalLine
+            // 
+            this.HorizontalLine.Enabled = true;
+            this.HorizontalLine.Tick += new System.EventHandler(this.HorizontalLine_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 590);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "/Total_Graph/0/Quickz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer Ellipse;
+        private System.Windows.Forms.Timer ReverseEllipse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer VertikalLine;
+        private System.Windows.Forms.Timer TotalGraph;
+        private System.Windows.Forms.Timer HorizontalLine;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ C# Graphics/Drawing/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x = 0;
+        int y = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int countloop;
+        int countloop2;
+        int line = 200;
+        int countvertline;
+        int countvertline2;
+        int total;
+        int rect0 = 50;
+        int rect1 = 330;
+        int rect2 = 225;
+        int rect3 = 225;
+        int rectcount;
+        int rectcount2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            countloop++;
+            countloop2++;
+            label1.Text = "Ellipse "+ countloop.ToString() +"/x";
+            label2.Text = "Ellipse "+countloop2.ToString() +"/20";
+            Graphics gg = panel1.CreateGraphics();
+            Pen pp = new Pen(Color.Green);
+            gg.DrawEllipse(pp, 50, 50, 100, 100);
+            gg.DrawEllipse(pp, 200, 50, 5, 5);
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red);
+            x = x + 5;
+            y = y + 5;
+            if (x == 100 & y == 100 & countloop2 == 20)
+            {
+                x = 0;
+                y = 0;
+                countloop2 = 0;
+                line = 200;
+                countvertline2 = 0;
+                rect0 = 50;
+                rect1 = 330;
+                rect2 = 225;
+                rect3 = 225;
+                rectcount2 = 0;
+                g.Clear(Color.Black);
+            }
+            g.DrawEllipse(p, 50, 50, x, y);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //reverse
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red);
+            x1 = x1 - 5;
+            y1 = y1 - 5;
+            if (x == 0 & y == 0)
+            {
+                x1 = 100;
+                y1 = 100;
+                g.Clear(Color.Black);            
+            }
+            g.DrawEllipse(p, 200, 50, x1, y1);
+        }
+
+        private void VertikalLine_Tick(object sender, EventArgs e)
+        {
+            Graphics ggg = panel1.CreateGraphics();
+            Pen ppp = new Pen(Color.Green);
+            line = line + 5;
+            countvertline++;
+            countvertline2++;
+            ggg.DrawLine(ppp, 600, line, 0, line);
+            label4.Text = "Vertikal Line " + countvertline.ToString() + "/x";
+            label3.Text = "Vertikal Line " + countvertline2.ToString() + "/20";
+        }
+
+        private void TotalGraph_Tick(object sender, EventArgs e)
+        {
+            total = countloop + countvertline + rectcount;
+            Text = "/Total_Graph/" + total.ToString();
+        }
+
+        private void HorizontalLine_Tick(object sender, EventArgs e)
+        {
+            ///50, 330, 200, 200
+            rectcount++;
+            rectcount2++;
+            rect0 = rect0 + 5;
+            rect1 = rect1 + 5;
+            rect2 = rect2 - 10;
+            rect3 = rect3 - 10;
+            Graphics gggg = panel1.CreateGraphics();
+            Pen pppp = new Pen(Color.Red);
+            Pen ppppp = new Pen(Color.Green);
+            gggg.DrawRectangle(ppppp, 150, 430, 25, 25);
+            gggg.DrawRectangle(pppp, rect0, rect1, rect2, rect3);
+            label6.Text = "Rectangle " + rectcount.ToString() + "/x";
+            label5.Text = "Rectangle " + rectcount2.ToString() + "/20";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
